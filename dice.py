654a166b2b793af406885c448c3cfb7accdb4e09
VarshaRavi11 <varsharavi111@gmail.com>
2016-07-11 19:46:13 -0400

    Add files via upload

--- dice.py
+++ dice.py
@@ -1,55 +1,67 @@
 import random
-i=0
-j=0
-s=0
+i=1
+j=1
+sr=0
+sc=0
+f=[[1 for x in range(9)] for y in range(9)]
+print(i,j)
 
-print (i,j)
-
-while ((i<8) and (j<8)):
-    print "The value is...."
-    val = random.randint(1,6)
-    print val
-    s=s+val
-    if(i%2 == 0):
+while ((i<=8) and (j<=8)):
+     print "The value is...."  
+     val = random.randint(1,6)
+     print val
+     if(i%2 != 0):
         if((j+val)<8):
-            j=j+val-1
+            j=j+val
         else:
-            x=7-j
+            x=8-j
             j=j+x
             i=i+1
-            y=val-x
-            j=j-y+1
-    elif(i==8):
+            y=val-x-1
+            j=j-y
+     elif(i==8):
         if(j-val>0):
             j=j-val
-        elif(j==0):
-            print (i,j)
-            print("you won") 
-            break
-        
+       
         else:
             x=j-1
-            if((j-val)>x-1):
-                continue
-            else:
-                j=j-x-1
-                y=val-x-1
-                j=j+y
-    else:
+            j=j-x
+            i=1
+            y=val-x-1
+            j=j+y
+     else:
         if((j-val)>0):
             j=j-val
-        else:
-            x=j
+        else:        
+            x=j-1
             j=j-x
             i=i+1
-            y=val-x 
+            y=val-x-1 
             j=j+y
                     
         
    
    
                     
-    print (i,j) 
-            
-                
+     print (i,j) 
+     f[i][j]=0
         
+     for i in range(1, 8):
+        for j in range(1, 8):
+             if(f[i][j]==0):
+                  sr+=1
+             elif(f[j][i]==0):
+                  sc+=1
+        else:
+            sc=0
+            sr=0
+    
+             
+     if(sr==8 or sc==8):
+         
+         print"BINGO"
+         break
+print f
+     
+              
+    
